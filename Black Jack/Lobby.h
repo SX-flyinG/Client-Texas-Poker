a7c3449d530f5e217efdfa56e0eea5282ead19f3
SX-flyinG <outcast9n0k@gmail.com>
2025-01-22 22:36:34 +0200

    Update Lobby.h

--- Black Jack/Lobby.h
+++ Black Jack/Lobby.h
@@ -1,19 +1,60 @@
 class UserLobby {
-    char buffer[1024]; // Buffer for server messages
+    char buffer[1024];
+    char choice;
 
 public:
-
-    void MainMenu(SOCKET clientSocket) {
+    int MainMenu(SOCKET clientSocket) {
         while (true) {
+            memset(buffer, 0, sizeof(buffer)); 
             int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
             if (bytesReceived > 0) {
-                buffer[bytesReceived] = '\0';
-                cout << buffer << endl;  // Отображение меню, полученного от сервера
-            }
-            else {
+                buffer[bytesReceived] = '\0'; 
+                cout << buffer << endl;
+            } else {
                 cerr << "Error: Failed to receive menu from server" << endl;
-                return;
+                return 1;
+            }
+
+            cout << "Enter your choice: ";
+            cin >> choice;
+            cin.ignore();
+
+            send(clientSocket, &choice, sizeof(choice), 0);
+
+            if (choice == '0' || choice == '3') {
+                memset(buffer, 0, sizeof(buffer));
+                int message = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+                if (message > 0) {
+                    buffer[message] = '\0';
+                    cout << buffer << endl;
+                } else {
+                    cerr << "Error: Failed to receive exit message from server" << endl;
+                    return 1;
+                }
+                break;
+            } else if (choice == '2') {
+                Rules(clientSocket); 
             }
         }
+        return 0;
+    }
+
+    void Rules(SOCKET clientSocket) {
+        memset(buffer, 0, sizeof(buffer)); 
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived > 0) {
+            buffer[bytesReceived] = '\0';
+            cout << buffer << endl;
+        } else {
+            cerr << "Error: Failed to receive rules menu from server" << endl;
+            return;
+        }
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        send(clientSocket, &choice, sizeof(choice), 0); 
+        MainMenu(clientSocket);
     }
 };
