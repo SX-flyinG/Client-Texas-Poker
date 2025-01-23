a3ca4e2259e31e47a7122808b86ae49c736610ff
l1k3r <Burya_ei79@student.itstep.org>
2025-01-23 17:55:55 +0200

    delete virtual mode from lobby

--- Black Jack/Lobby.h
+++ Black Jack/Lobby.h
@@ -1,25 +1,17 @@
-// Базовый абстрактный класс
-class IMenu {
-public:
-    virtual int MainMenu(SOCKET clientSocket) = 0;
-    virtual void Rules(SOCKET clientSocket) = 0;
-    virtual ~IMenu() = default; // Виртуальный деструктор для безопасности
-};
-
-// Класс UserLobby реализует интерфейс IMenu
-class UserLobby : public IMenu {
+class UserLobby {
     char buffer[1024];
     char choice;
 
 public:
-    int MainMenu(SOCKET clientSocket) override {
+    int MainMenu(SOCKET clientSocket) {
         while (true) {
-            memset(buffer, 0, sizeof(buffer)); 
+            memset(buffer, 0, sizeof(buffer));
             int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
             if (bytesReceived > 0) {
-                buffer[bytesReceived] = '\0'; 
+                buffer[bytesReceived] = '\0';
                 cout << buffer << endl;
-            } else {
+            }
+            else {
                 cerr << "Error: Failed to receive menu from server" << endl;
                 return 1;
             }
@@ -36,25 +28,28 @@ public:
                 if (message > 0) {
                     buffer[message] = '\0';
                     cout << buffer << endl;
-                } else {
+                }
+                else {
                     cerr << "Error: Failed to receive exit message from server" << endl;
                     return 1;
                 }
                 break;
-            } else if (choice == '2') {
-                Rules(clientSocket); 
+            }
+            else if (choice == '2') {
+                Rules(clientSocket);
             }
         }
         return 0;
     }
 
-    void Rules(SOCKET clientSocket) override {
-        memset(buffer, 0, sizeof(buffer)); 
+    void Rules(SOCKET clientSocket) {
+        memset(buffer, 0, sizeof(buffer));
         int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
         if (bytesReceived > 0) {
             buffer[bytesReceived] = '\0';
             cout << buffer << endl;
-        } else {
+        }
+        else {
             cerr << "Error: Failed to receive rules menu from server" << endl;
             return;
         }
@@ -63,7 +58,7 @@ public:
         cin >> choice;
         cin.ignore();
 
-        send(clientSocket, &choice, sizeof(choice), 0); 
+        send(clientSocket, &choice, sizeof(choice), 0);
         MainMenu(clientSocket);
     }
 };
